d295356e3e2bc93a80e202f80ee578ed4a91adce
Felix Ruppert <ruppert@is.mpg.de>
2017-10-23 14:09:34 +0200

    Add power plots to plotLoadcase.m

--- plotLoadcase.m
+++ plotLoadcase.m
@@ -5,11 +5,11 @@ time=(0:length(accelPoints)-1)*timeStep;
 
 % plot dynamics
 figure
-subplot(2,1,1); plot(time,accelPoints,'b');
+subplot(3,1,1); plot(time,accelPoints,'b');
 hold on;
-subplot(2,1,1); plot(time,velocity,'k:','linewidth',1.5);
+subplot(3,1,1); plot(time,velocity,'k:','linewidth',1.5);
 hold on
-subplot(2,1,1); plot(time,position,'g','linewidth',1.5);
+subplot(3,1,1); plot(time,position,'g','linewidth',1.5);
 %grid;
 set(gca,'Fontsize',12)
 xlabel('time [s]')
@@ -23,12 +23,12 @@ torqueLoadMax = max(abs(torquePoints));
 torqueLoadRMS = sqrt(sum(torquePoints.^2*timeStep)/(timeStep*length(torquePoints)));
 
 
-subplot(2,1,2);plot(time,torquePoints,'linewidth',1.5);
+subplot(3,1,2);plot(time,torquePoints,'linewidth',1.5);
 hold on;
 temp=ones(size(torquePoints))*torqueLoadRMS;
-subplot(2,1,2);line(time,temp,'Color','g','LineStyle','--');
+subplot(3,1,2);line(time,temp,'Color','g','LineStyle','--');
 temp=ones(size(torquePoints))*torqueLoadMax;
-subplot(2,1,2);line(time,temp,'Color','r','LineStyle','-.');
+subplot(3,1,2);line(time,temp,'Color','r','LineStyle','-.');
 
 set(gca,'Fontsize',12)
 %grid;
@@ -37,3 +37,19 @@ ylabel('torque [Nm]')
 set(gca,'Fontsize',10)
 legend('Load Torque', ['T_{RMS} ', num2str(torqueLoadRMS(1)),' Nm'], ['T_{Peak} ', num2str(torqueLoadMax(1)),' Nm'])
 
+% plot power
+powerLoad= torquePoints.*velocity;
+powerMean= norm(powerLoad,1)/length(powerLoad);
+powerMax= max(powerLoad);
+
+subplot(3,1,3);
+plot(time,powerLoad,'linewidth',1.5);
+hold on;
+%grid;
+hold on
+line(time,ones(size(time))*powerMean,'Color','g','LineStyle','--')
+line(time,ones(size(time))*powerMax,'Color','r','LineStyle','-.')
+title('Power as function of time');
+xlabel('Time [s]')
+ylabel('Power [W]')
+legend('Power','Mean Power','Max Power');
