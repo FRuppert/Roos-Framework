30338a00cd29745b00c3f9b175c60194753ea967
Felix Ruppert <ruppert@is.mpg.de>
2017-10-27 08:15:14 +0200

    Add constant inertia option to calculateGearboxInertia for Roos paper example

--- calculateGearboxInertia.m
+++ calculateGearboxInertia.m
@@ -5,27 +5,39 @@ function [gearbox]= calculateGearboxInertia
 % output:
 % gearbox(1) = gear ratios
 % gearbox(2) = respective gearbox inertia
-[gearData,teeth]=getGearData;
-radius=teeth.*gearData(4,:)*1e-3;
-for idx1=1:length(teeth)
-   for idx2=1:length(teeth)
-       possibleRatios(idx1,idx2)=teeth(idx2)/teeth(idx1);
-       
-       gearboxInertia(idx1,idx2)=  0.5*pi*radius(idx1)^2*gearData(3)*gearData(5)*radius(idx1)^2 ...
-                                +0.5*pi*radius(idx2)^2*gearData(3)*gearData(5)*radius(idx2)^2; 
-       
-   end
-end
-%reshape matrix into vectors
-gearbox(1,:)=reshape(possibleRatios,[1,length(radius)^2]);
-gearbox(2,:)=reshape(gearboxInertia,[1,length(radius)^2]);
-%sort gear ratios
-[gearbox(1,:),index]=sort(gearbox(1,:));
-% sort the inertias by the index of the ratio sorting
-for idx=1:length(index)
- gearboxTemp(idx)=gearbox(2,index(idx));
-end
-gearbox(2,:)=gearboxTemp;
+% [gearData,teeth]=getGearData;
+% radius=teeth.*gearData(4,:)*1e-3;
+% for idx1=1:length(teeth)
+%    for idx2=1:length(teeth)
+%        possibleRatios(idx1,idx2)=teeth(idx2)/teeth(idx1);
+%        
+%        gearboxInertia(idx1,idx2)=  0.5*pi*radius(idx1)^2*gearData(3)*gearData(5)*radius(idx1)^2 ...
+%                                 +0.5*pi*radius(idx2)^2*gearData(3)*gearData(5)*radius(idx2)^2; 
+%        
+%    end
+% end
+% %reshape matrix into vectors
+% gearbox(1,:)=reshape(possibleRatios,[1,length(radius)^2]);
+% gearbox(2,:)=reshape(gearboxInertia,[1,length(radius)^2]);
+% %sort gear ratios
+% [gearbox(1,:),index]=sort(gearbox(1,:));
+% % sort the inertias by the index of the ratio sorting
+% for idx=1:length(index)
+%  gearboxTemp(idx)=gearbox(2,index(idx));
+% end
+% gearbox(2,:)=gearboxTemp;
+
+
+%% Default Roos value:
+inertiaGearbox=0.0;
+gearRatioResolution=0.1;
+gearRatioMin=0.1;
+gearRatioMax=40;
+
+gearbox(1,:)=[gearRatioMin:gearRatioResolution:gearRatioMax];
+gearbox(2,:)=ones(1,size(gearbox,2))*inertiaGearbox;
+
+
 
 
 
