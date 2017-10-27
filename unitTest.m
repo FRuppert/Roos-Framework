d78a7251b4e2519380733af55976687407dba073
Felix Ruppert <ruppert@is.mpg.de>
2017-10-27 08:14:39 +0200

    Add loadcase from Roos paper to unitTest

--- unitTest.m
+++ unitTest.m
@@ -2,25 +2,30 @@ clear all
 clc
 % roos original acceleration data
 timeStep=0.01;
-inertiaLoad=0.4;
-accelerationValue=20;
-accelPoints(1:20) = 0;
-accelPoints(21:30) = accelerationValue;
-accelPoints(31:40) = -accelerationValue;
-accelPoints(41:60) = 0;
-accelPoints(61:70) = -accelerationValue;
-accelPoints(71:80) = accelerationValue;
-accelPoints(81:120) = 0;
-accelPoints(121:130) = -accelerationValue;
-accelPoints(131:140) = accelerationValue;
-accelPoints(141:160) = 0;
-accelPoints(161:170) = accelerationValue;
-accelPoints(171:180) = -accelerationValue;
-accelPoints(181:201) = 0;
+inertiaLoad=0.012;
+torqueFriction=7.5;
+Acc=350;
+accelPoints(1:40) = 0;
+accelPoints(51:70) = Acc;
+accelPoints(71:100) = 0;
+accelPoints(101:120) = -Acc;
+accelPoints(121:160) = 0;
+accelPoints(161:180) = -Acc;
+accelPoints(181:210) = 0;
+accelPoints(211:230) = Acc;
+accelPoints(231:300) = 0;
+accelPoints(301:320) = -Acc;
+accelPoints(321:330) = 0;
+accelPoints(331:350) = Acc;
+accelPoints(351:390) = 0;
+accelPoints(391:410) = +Acc;
+accelPoints(411:420) = 0;
+accelPoints(421:440) = -Acc;
+accelPoints(441:501) = 0;
 
 %Roos original torque data
 [velocity, position]=integrateAcceleration(accelPoints,timeStep);
-torquePoints=inertiaLoad*accelPoints;
+torquePoints=sign(velocity).*torqueFriction+inertiaLoad*accelPoints+7.5;
 
 
 
