f8adba055efb551929fd1747f9df1d29d86665a7
Felix Ruppert <ruppert@is.mpg.de>
2018-01-23 09:16:10 +0100

    working version, removed original scripts

--- CandidateGearData.m
+++
@@ -1,9 +0,0 @@
-%Candidate gear data file.
-%Fredrik Roos, December 2004.
-maxGearRPM = 6000; %Max ang. vel (motor side) RPM
-gearEff = 0.97;    %Efficiency
-Jg=0;               %Gear inertia
-Ngear = [3 4 5 6 7 8 9]; %Gear ratio vector
-
-
-
